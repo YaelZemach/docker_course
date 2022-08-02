6ce5cb2b947f27861916833984272d96e2cdc03a
YaelZemach <y0548561558@gmail.com>
2022-08-02 16:01:26 +0300

    change syntax

---
+++  k8s-course/ex-1/comands.sh
@@ -0,0 +1,11 @@
+# get the status of the nodes
+kubectl get nodes
+
+#get the pods
+kubectl get pods
+
+#get the services
+kubectl get services
+
+#create the deployment 
+kubectl create deployment private-name-image name-image
